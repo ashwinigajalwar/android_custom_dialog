b39fe1aa80cc662c92d80214938393d5330ac2ee
50797_Ashwini <ashwinigajalwar@gmail.com>
2024-10-23 23:50:44 +0530

    Implemented Custom Dialog.

--- app/src/main/java/com/example/implement_custom_dialog/CustomDialog.java
+++ app/src/main/java/com/example/implement_custom_dialog/CustomDialog.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -43,10 +44,41 @@ public class CustomDialog extends Dialog {
 
 
     private void initListeners(){
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                double total=0;
+                String currencyText="";
+
+
+                if (checkedId==R.id.radio_can){
+                    total = amount * 0.016; // Conversion rate for Canada
+                    currencyText = "CANADA=";
+
+                } else if (checkedId==R.id.radio_usa) {
+                    total = amount * 0.012; // Conversion rate for usa
+                    currencyText = "USA=";
+
+                } else if (checkedId==R.id.radio_uk) {
+                    total = amount * 0.0091 ; // Conversion rate for uk
+                    currencyText = "UK=";
+
+                } else if (checkedId==R.id.radio_chi) {
+                    total = amount * 0.085; // Conversion rate for China
+                    currencyText = "CHINA=";
+
+                }
+
+                txtView.setText(currencyText+total);
+            }
+        });
+
         okBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                Toast.makeText(getContext(), "Back to Home Screen", Toast.LENGTH_SHORT).show();
                 dismiss();
             }
         });
